88a18106125ab5fe42545ce21b939114145ba44c
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-03-05 22:42:16 +0300

    Adds functions

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+   return `This Saturday, I want to ${activity}!`;
+   
+}
+
+console.log(saturdayFun());
+console.log(saturdayFun("sleep"))
+console.log(saturdayFun("game"))
+
+
+
+function mondayWork (toDo = "go to the office") {
+    return `This Monday, I will ${toDo}.`
+}
+console.log(mondayWork());
+console.log(mondayWork("dance"));
+
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`; 
+    }
+}
+
+const excitement = wrapAdjective("&&");
+console.log(excitement("an Aries"));
+
+
+
+// This Saturday, I want to roller-skate!
+// This Saturday, I want to sleep!
+// This Saturday, I want to game!
+// This Monday, I will go to the office.
+// This Monday, I will dance.
+// You are &&an Aries&&!
